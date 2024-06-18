e29da117184e98e1e9c2e7eb5a5753c512ff60de
keremduman30 <berwari30_1907@hotmail.com>
2024-06-18 21:21:33 +0300

    improved crud operations in backend

--- backend/src/routers/books.js
+++ backend/src/routers/books.js
@@ -1,8 +1,39 @@
 import express from "express";
+import db from "../config/database.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json("starting app");
+  const q = "SELECT * FROM books";
+  db.query(q, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
+  });
+});
+
+router.post("/add", (req, res) => {
+  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?,?,?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  db.query(q, values, (err, data) => {
+    if (err) return res.json(err);
+    return res.json("book has been created");
+  });
+});
+router.delete("/delete", (req, res) => {
+  const { id } = req.body;
+  const q = "delete from books where id=? ";
+  db.query(q, [id], (err, result) => {
+    if (err) return res.json(err);
+    return res.json("the book deleted successfull");
+  });
+});
+router.put("/update", (req, res) => {
+  const q = "UPDATE books SET title = ?, `desc` = ?, cover = ? WHERE id = ?";
+  const { title, desc, cover, id } = req.body;
+  const values = [title, desc, cover, id];
+  db.query(q, values, (err, result) => {
+    if (err) return res.json(err);
+    return res.json("the book updated successfull");
+  });
 });
 
 export default router;
